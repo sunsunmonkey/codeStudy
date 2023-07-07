a9e6ff86987facf6a1a80d048cb32a8f1752a9a6
sunsunmonkey <2933201543@qq.com>
2023-07-07 14:40:57 +0800

    emit-test

--- vue-study/src/test/App.vue
+++ vue-study/src/test/App.vue
@@ -1,15 +1,20 @@
 <template>
     <h1 @click="console.log($event)">11111</h1>
+    <Son @son='($event)=>console.log($event)'></Son>
 </template>
 
 <script>
+import Son from './Son.vue';
 export default{
     name:"App",
     data(){
         return{
-          
+         
             mes: ''
         }
+    },
+    components:{
+        Son 
     }
 }
 </script>
---
+++ vue-study/src/test/Son.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 @click="handleSon">son</h1>
+</template>
+
+<script>
+export default{
+    name:"Son",
+    emits:['son'],
+    methods:{
+        handleSon(){
+            this.$emit('son',100,2)
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    
+</style>
