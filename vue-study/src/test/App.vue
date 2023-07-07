84a79d94cba0ddae21a54926339db3c9d1cc6d98
sunsunmonkey <2933201543@qq.com>
2023-07-07 11:56:45 +0800

    demotest

---
+++ vue-study/src/test/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 @click="console.log($event)">11111</h1>
+</template>
+
+<script>
+export default{
+    name:"App",
+    data(){
+        return{
+          
+            mes: ''
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    
+</style>
