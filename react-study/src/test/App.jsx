f817a0e7831bdea2685b02a1f81a9a8d25a6d342
sunsunmonkey <2933201543@qq.com>
2023-07-12 19:51:37 +0800

    test

--- react-study/src/test/App.jsx
+++ react-study/src/test/App.jsx
@@ -1,16 +1,16 @@
-import React, { Component, memo, useState } from 'react'
+import React, {  memo, useState } from 'react'
 //rmc memo function
-const App = memo(function () {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div className="">   <button onClick={e=>{setCounter(counter+1)} }>{counter}</button><J count={1}></J></div>
+import Son from './Son'
+const App = memo(() => {
+  const [name,setname] = useState('zzk')
 
-    )
+  return (
+    <div className="" >
+        <div onClick={()=>{setname('zk')}}>App</div>
+        <Son name={name}></Son>
+    </div>
+    
+  )
 })
-const J  =memo(function J() {
-    console.log("render");
-    return <h1>1616</h1>
-}
-)
 
 export default App;
---
+++ react-study/src/test/Son.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Son = (props) => {
+  const {name} = props
+  console.log('render')  
+  return (
+    <div>111</div>
+  )
+}
+
+export default Son
