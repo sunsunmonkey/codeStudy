565e475ef246f498bd59f0e5ee44a2028e851bd5
sunsunmonkey <2933201543@qq.com>
2023-07-14 15:18:41 +0800

    test

--- vue-study/src/main.js
+++ vue-study/src/main.js
@@ -3,16 +3,16 @@ import { createApp } from 'vue'
 // import App   from  "@/父子通信/App"
 // import App from  './slot/App'
 // import App from './provide&inject/App'
-// import App from './test/App'
+import App from './test/App'
 // import App from './Composition API/App'
 // import App from './路由/App.vue'
 // import router from './路由/router'
 // import App from './vuex/App.vue'
 // import store from './vuex/store'
-import App from './pinia/App.vue'
-import store from './pinia/store'
+// import App from './pinia/App.vue'
+// import store from './pinia/store'
 const app = createApp(App)
 // app.use( router)
 // app.use(store)
-app.use(store)
+// app.use(store)
 app.mount('#app')
--- vue-study/src/test/App.vue
+++ vue-study/src/test/App.vue
@@ -1,19 +1,20 @@
 <template>
-    <h1 @click="console.log($event)">11111</h1>
-    <Son @son='($event)=>console.log($event)' ></Son>
-    <Son  ></Son>
+    <button  @click="openDrawer">打开</button>
+    <tiny-drawer v-model:visible="drawerVisible" title="OpenTiny是什么" dragable>
+        <!-- 默认插槽的内容是可以自行定义，比如：可以是表格行的详情内容 -->
+        <p>OpenTiny 是一套华为云出品的企业级组件库解决方案，适配 PC 端 / 移动端等多端，涵盖 Vue2 / Vue3 / Angular 多技术栈，拥有主题配置系统 / 中后台模板 / CLI 命令行等效率提升工具，可帮助开发者高效开发 Web 应用。</p>
+    </tiny-drawer>
 </template>
 
-<script>
-import Son from './Son.vue';
-export default{
-    name:"App",
-    components:{
-        Son 
-    },
+<script setup>
+import TinyDrawer from '@opentiny/vue-drawer'
+import { ref } from 'vue';
+
+const drawerVisible = ref(false)
+const openDrawer = () => {
+  drawerVisible.value = true
 }
 </script>
-
 
 <style scoped>
     
