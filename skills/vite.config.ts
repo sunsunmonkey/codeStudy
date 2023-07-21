fefd73e907cd31d4f84c995028e8de04b06e82c1
sunsunmonkey <2933201543@qq.com>
2023-07-21 19:52:47 +0800

    feat: prettierr

--- skills/vite.config.ts
+++ skills/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import postcssPresetEnv from "postcss-preset-env" 
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import postcssPresetEnv from 'postcss-preset-env';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 // https://vitejs.dev/config/
 export default defineConfig({
-  css:{
-    postcss:{
-      plugins:[postcssPresetEnv(),tailwindcss(),autoprefixer()]
-    },
-    preprocessorOptions: {
-      less: {
-        math: "always",
-      },
-    }
-  },
-  plugins: [react()],
-})
+	css: {
+		postcss: {
+			plugins: [postcssPresetEnv(), tailwindcss(), autoprefixer()]
+		},
+		preprocessorOptions: {
+			less: {
+				math: 'always'
+			}
+		}
+	},
+	plugins: [react()]
+});
