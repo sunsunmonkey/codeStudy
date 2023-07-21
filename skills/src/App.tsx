863e7595bcebde62ccc5bf56cd56764e4724d5e8
sunsunmonkey <2933201543@qq.com>
2023-07-21 16:20:35 +0800

    tailwindcss

--- skills/src/App.tsx
+++ skills/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+     <h1 className="text-3xl font-bold underline bg-black">
+      Hello world!
+    </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className={styles.logo} alt="Vite logo" />
--- skills/vite.config.ts
+++ skills/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import postcssPresetEnv from "postcss-preset-env" 
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   css:{
     postcss:{
-      plugins:[postcssPresetEnv()]
+      plugins:[postcssPresetEnv(),tailwindcss(),autoprefixer()]
     },
     preprocessorOptions: {
       less: {
