28f7d7bf6a0518a07119d56016a2b234a6242318
sunsunmonkey <2933201543@qq.com>
2023-08-08 20:24:14 +0800

    feat: vue-webpack

---
+++ vue-webpack/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "Hello World" };
+  },
+};
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+</style>
